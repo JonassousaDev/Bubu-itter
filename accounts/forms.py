effa28c1082fab2f7e5e034284218cdad31d4b0f
JonasSousa <jonassousa.dev@gmail.com>
2023-01-07 20:47:59 +0000

    more css

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,9 +7,26 @@ class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
-        model =User
-        fields = ['username','email','password1','password2']
-
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        # labels = {
+        #     'username': 'Username',
+        #     'email': '',
+        #     'password1': '',
+        #     'password2': '',
+        # }
+        # widgets = {
+        #     'username': forms.TextInput(attrs={'placeholder': 'Enter your username'}),
+        #     'email': forms.TextInput(attrs={'placeholder': 'Enter your email'}),
+        #     'password1': forms.PasswordInput(attrs={'placeholder': 'Enter your password'}),
+        #     'password2': forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}),
+        # }
+        # help_texts = {
+        #     'username': '',
+        #     'email': '',
+        #     'password1': '',
+        #     'password2': '',
+        # }
 
 class ProfileUpdateForm(forms.ModelForm):
     
